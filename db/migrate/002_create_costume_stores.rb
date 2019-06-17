de3c6e4f47fe9bc71db7ad3860774078f851c8be
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-17 13:42:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,7 +10,7 @@ class CreateCostume_stores < ActiveRecord::Migration[4.2]
       t.datetime :opening_time
       t.datetime :closing_time
 
-      t.timestamps 
+      t.timestamps
     end
   end
 end
