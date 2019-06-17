cb1874edc53f83133a19caf097c9883b632e1675
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-17 13:52:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouse < ActiveRecord::Migration[4.2]
   create_table :haunted_houses do |t|
-    t.string :name
+      t.string :name
       t.string :location
       t.float :price
       t.string :theme
