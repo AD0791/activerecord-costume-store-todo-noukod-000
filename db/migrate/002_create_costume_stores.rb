feef10e979644bee8f83f59f7b0893a9ddc62a5a
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-17 13:52:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,4 @@
-# Create your costume_stores migration here
-class CreateCostume_stores < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
@@ -9,6 +8,7 @@ class CreateCostume_stores < ActiveRecord::Migration[4.2]
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
+
       t.timestamps null: false
     end
   end
